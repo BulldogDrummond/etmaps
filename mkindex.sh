45352834669298d8e9618aad9af721147927964d
Bulldog Drummond <butchdrummond@gmail.com>
2013-02-20 09:35:38 -0500

    Mod mkindex script for additional data.

--- mkindex.sh
+++ mkindex.sh
@@ -2,7 +2,7 @@
 
 echo "<html>"
 echo "<head>"
-echo "<title>RedStar:ET Downloads</title>"
+echo "<title>Redstar:ET Downloads</title>"
 echo "</head>"
 echo "<body>"
 echo "<h3>Map Archive</h3>"
@@ -10,7 +10,7 @@ echo "<hr>"
 echo "<br>"
 echo "<center>"
 echo "<table>"
-echo "<tr><td>Map ID</td><td>Map Name</td><td>Region</td><td>Conditions</td><td>Filename</td></tr>"
+echo "<tr><td>Map ID</td><td>Name</td><td>Map Name</td><td>Region</td><td>Conditions</td><td>Filename</td></tr>"
 
 DNUM=1
 for MAPIDX in `cat mapindex.dat | grep -v _CODE`
@@ -21,21 +21,21 @@ do
   COMPASS=`echo $MAPIDX | awk -F: '{print $4}'`
   COMP_CODE=`echo $MAPIDX | awk -F: '{print $5}'`
   NIGHT=`echo $MAPIDX | awk -F: '{print $6}'`
-  WEATHER=`echo $MAPIDX | awk -F: '{print $7}'`
-  MAP_NAME=`echo $MAPIDX | awk -F: '{print $8}'`
-  FILE_NAME=`echo $MAPIDX | awk -F: '{print $9}'`
-  if [ $NIGHT = "1" ]; then
-    DAYLIGHT="NIGHT"
-  else
-    DAYLIGHT="DAY"
-  fi
+  MTIME=`echo $MAPIDX | awk -F: '{print $7}'`
+  WEATHER=`echo $MAPIDX | awk -F: '{print $8}'`
+  MAP_NAME=`echo $MAPIDX | awk -F: '{print $9}'`
+  MAP_LNAME=`echo $MAPIDX | awk -F: '{print $10}' | tr "_" " "`
+  FILE_NAME=`echo $MAPIDX | awk -F: '{print $11}'`
+  TLIMIT=`echo $MAPIDX | awk -F: '{print $12}'`
+  DEFEND=`echo $MAPIDX | awk -F: '{print $13}'`
   echo "<tr><td>${REGION_CODE}-${ORDER}</td><td>"
+  echo "<td>${MAP_LNAME}</td><td>"
   if [ ${FILE_NAME} = "BUILTIN" ]; then
     echo "${MAP_NAME}"
   else
     echo "<a href=${MAP_NAME}.html>${MAP_NAME}</a>"
   fi
-  echo "</td><td>${REGION_NAME} / ${COMPASS}</td><td>${DAYLIGHT}/${WEATHER}</td><td>"
+  echo "</td><td>${REGION_NAME} / ${COMPASS}</td><td>${MTIME}/${WEATHER}</td><td>"
   if [ ${FILE_NAME} = "BUILTIN" ]; then
     echo "${FILE_NAME}"
   else
@@ -49,7 +49,7 @@ echo "</table>"
 echo "</center>"
 echo "<br>"
 echo "<hr>"
-echo "<center>RedStar:ET</center>"
+echo "<center>Redstar:ET</center>"
 echo "</body>"
 echo "</html>"
 
