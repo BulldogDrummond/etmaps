e763ebc3dfecc3fe525166e9647ed6fca395439e
Bulldog Drummond <butchdrummond@gmail.com>
2013-02-23 10:45:34 -0500

    Mod mkindex script.  Add generated web index.html.

--- mkindex.sh
+++ mkindex.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-echo "<html>"
-echo "<head>"
-echo "<title>Redstar:ET Downloads</title>"
-echo "</head>"
-echo "<body>"
-echo "<h3>Map Archive</h3>"
-echo "<hr>"
-echo "<br>"
-echo "<center>"
-echo "<table>"
-echo "<tr><td>Map ID</td><td>Name</td><td>Map Name</td><td>Region</td><td>Conditions</td><td>Filename</td></tr>"
+IDXHTML="index.html"
+
+echo "<html>" > $IDXHTML
+echo "<head>" >> $IDXHTML
+echo "<title>Redstar:ET Downloads</title>" >> $IDXHTML
+echo "</head>" >> $IDXHTML
+echo "<body>" >> $IDXHTML
+echo "<h3>Map Archive</h3>" >> $IDXHTML
+echo "<hr>" >> $IDXHTML
+echo "<br>" >> $IDXHTML
+echo "<center>" >> $IDXHTML
+echo "<table>" >> $IDXHTML
+echo "<tr><td>Map ID</td><td>Name</td><td>Map Name</td><td>Region</td><td>Conditions</td><td>Filename</td></tr>" >> $IDXHTML
 
 DNUM=1
 for MAPIDX in `cat mapindex.dat | grep -v _CODE`
@@ -28,29 +30,29 @@ do
   FILE_NAME=`echo $MAPIDX | awk -F: '{print $11}'`
   TLIMIT=`echo $MAPIDX | awk -F: '{print $12}'`
   DEFEND=`echo $MAPIDX | awk -F: '{print $13}'`
-  echo "<tr><td>${REGION_CODE}-${ORDER}</td><td>"
-  echo "<td>${MAP_LNAME}</td><td>"
+  echo "<tr><td>${REGION_CODE}-${ORDER}</td><td>" >> $IDXHTML
+  echo "<td>${MAP_LNAME}</td><td>" >> $IDXHTML
   if [ ${FILE_NAME} = "BUILTIN" ]; then
-    echo "${MAP_NAME}"
+    echo "<a href=maps/BUILTIN/${MAP_NAME}.html>${MAP_NAME}</a>" >> $IDXHTML
   else
-    echo "<a href=${MAP_NAME}.html>${MAP_NAME}</a>"
+    echo "<a href=maps/${MAP_NAME}/${MAP_NAME}.html>${MAP_NAME}</a>" >> $IDXHTML
   fi
-  echo "</td><td>${REGION_NAME} / ${COMPASS}</td><td>${MTIME}/${WEATHER}</td><td>"
+  echo "</td><td>${REGION_NAME} / ${COMPASS}</td><td>${MTIME}/${WEATHER}</td><td>" >> $IDXHTML
   if [ ${FILE_NAME} = "BUILTIN" ]; then
-    echo "${FILE_NAME}"
+    echo "${FILE_NAME}" >> $IDXHTML
   else
-    echo "<a href=${FILE_NAME}>${FILE_NAME}</a>"
+    echo "<a href=maps/${MAP_NAME}/${FILE_NAME}>${FILE_NAME}</a>" >> $IDXHTML
   fi
-  echo "</td></tr>"
+  echo "</td></tr>" >> $IDXHTML
   DNUM=$NEXTD
 done
 
-echo "</table>"
-echo "</center>"
-echo "<br>"
-echo "<hr>"
-echo "<center>Redstar:ET</center>"
-echo "</body>"
-echo "</html>"
+echo "</table>" >> $IDXHTML
+echo "</center>" >> $IDXHTML
+echo "<br>" >> $IDXHTML
+echo "<hr>" >> $IDXHTML
+echo "<center>Redstar:ET</center>" >> $IDXHTML
+echo "</body>" >> $IDXHTML
+echo "</html>" >> $IDXHTML
 
 # EOF
