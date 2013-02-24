fa890a895b62cf76e1fa481c9330f33e01ad77b4
Bulldog Drummond <butchdrummond@gmail.com>
2013-02-24 11:27:51 -0500

    Beautification: Phase III

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
+<html>
+<head>
+<title>Redstar:ET - Map Archive</title>
 <link href='css/maps.css' rel='stylesheet' type='text/css' />
+</head>
+<body>
 <div class=mainbody>
   <div class=LayoutIndex_header>
     <br>
     <h2>Redstar:ET - Map Archive</h2>
   </div>
-  <div class=LayoutIndex_1>
-<?php include "mapindex.inc.php"; ?>
+  <div class=LayoutIndex_main>
+    <?php include "mapindex.inc.php"; ?>
   </div>
-  <div class=LayoutIndex_2><br><h5>Copyright &copy; 2013 - Redstar:ET</h5></div> 
+  <div class=LayoutIndex_footer><br><h5>Copyright &copy; 2013 - Redstar:ET</h5></div> 
 </div>
+</body>
+</html>
--- mkmappage.sh
+++ mkmappage.sh
@@ -20,7 +20,7 @@ do
   else
     cd maps/${MAP_NAME}
   fi
-  MAP_BRIEFING=`cat ${MAP_NAME}.desc | tr -d "\n"`
+  MAP_BRIEFING=`cat ${MAP_NAME}.desc`
   MAP_HTML="${MAP_NAME}.html"
   MAP_IMG="${MAP_NAME}.jpg"
   MAP_CCIMG="${MAP_NAME}_cc.jpg"
@@ -30,32 +30,47 @@ do
   echo "<html>" > ${MAP_HTML}
   echo "<head>" >> ${MAP_HTML}
   echo "<title>Redstar:ET - Map: ${MAP_LONGNAME}</title>" >> ${MAP_HTML}
+  echo "<link href='/etmaps/css/maps.css' rel='stylesheet' type='text/css' />" >> ${MAP_HTML}
   echo "</head>" >> ${MAP_HTML}
   echo "<body>" >> ${MAP_HTML}
-  echo "<center>" >> ${MAP_HTML}
-  echo "<table width=800>" >> ${MAP_HTML}
-  echo "<tr><td colspan=2 align=center><h3>Map: ${MAPID} - ${MAP_LONGNAME}</h3></td></tr>" >> ${MAP_HTML}
-  echo "<tr><td align=right><IMG SRC=${MAP_IMG}></td><td><IMG SRC=${MAP_CCIMG}></td></tr>" >> ${MAP_HTML}
-  echo "</table>" >> ${MAP_HTML}
-  echo "<table width=800>" >> ${MAP_HTML}
-  echo "<tr><td>ID: ${MAPID}</td><td>${MAP_LONGNAME} (${MAP_NAME})</td></tr>" >> ${MAP_HTML}
-  echo "<tr><td>Region:</td><td>${REGION_NAME} (${REGION_CODE}) / ${COMPASS}</td></tr>" >> ${MAP_HTML}
-  echo "<tr><td>Conditions:</td><td>${MTIME} / ${WEATHER}</td></tr>" >> ${MAP_HTML}
-  echo "<tr><td>Defending:</td><td>${DEFEND}</td></tr>" >> ${MAP_HTML}
-  echo "<tr><td colspan=2>&nbsp;</td></tr>" >> ${MAP_HTML}
-  echo "<tr><td colspan=2>Briefing:</td></tr>" >> ${MAP_HTML}
-  echo "<tr><td colspan=2>${MAP_BRIEFING}</td></tr>" >> ${MAP_HTML} 
-  echo "<tr><td colspan=2><br>Time Limit: ${TLIMIT} minutes<br>&nbsp;</td></tr>" >> ${MAP_HTML}
+  echo "<div class=mainbody>" >> ${MAP_HTML}
+  echo "  <div class=LayoutMap_header>" >> ${MAP_HTML}
+  echo "    <br>" >> ${MAP_HTML}
+  echo "    <h2>Map: ${MAP_LONGNAME} / ${MAPID}</h2>" >> ${MAP_HTML}
+  echo "  </div>" >> ${MAP_HTML}
+  echo "  <div class=LayoutMap_briefing>" >> ${MAP_HTML}
+  echo "    <h4>Overview:</h4>" >> ${MAP_HTML}
+  echo "    <p class=\"typewriter\">" >> ${MAP_HTML}
+  echo "      <table width=90%>" >> ${MAP_HTML}
+  echo "        <tr><td>ID:</td><td>${MAPID} ${MAP_LONGNAME} (${MAP_NAME})</td></tr>" >> ${MAP_HTML}
+  echo "        <tr><td>Region:</td><td>${REGION_NAME} (${REGION_CODE}) / ${COMPASS}</td></tr>" >> ${MAP_HTML}
+  echo "        <tr><td>Conditions:</td><td>${MTIME} / ${WEATHER}</td></tr>" >> ${MAP_HTML}
+  echo "        <tr><td>Defending:</td><td>${DEFEND}</td></tr>" >> ${MAP_HTML}
+  echo "        <tr><td>Time Limit:</td><td>${TLIMIT} minutes</td></tr>" >> ${MAP_HTML}
+  echo "      </table>" >> ${MAP_HTML}
+  echo "    </p>" >> ${MAP_HTML}
+  echo "    <h4>Briefing:</h4>" >> ${MAP_HTML}
+  echo "    <p class=\"typewriter\">" >> ${MAP_HTML}
+  echo "    ${MAP_BRIEFING}" >> ${MAP_HTML}
+  echo "    </p>" >> ${MAP_HTML}
+  echo "    <h4>Links:</h4>" >> ${MAP_HTML}
+  echo "    <p class=\"typewriter\">" >> ${MAP_HTML}
+  echo "      <table width=90%>" >> ${MAP_HTML}
   if [ $FILE_NAME = "BUILTIN" ]; then
-    echo "<tr><td colspan=2>This map is included.</td></tr>" >> ${MAP_HTML}
-    echo "<tr><td>Omni-Bot File:</td><td><a href=BUILTIN-omnibot.tgz>BUILTIN-omnibot.tgz</a></td></tr>" >> ${MAP_HTML}
+    echo "        <tr><td>Map File:</td><td>This map is included.</td></tr>" >> ${MAP_HTML}
+    echo "        <tr><td>Omni-Bot File:</td><td><a href=BUILTIN-omnibot.tgz>BUILTIN-omnibot.tgz</a></td></tr>" >> ${MAP_HTML}
   else
-    echo "<tr><td>Map File:</td><td><a href=${MAP_FILE}>${MAP_FILE}</a></td></tr>" >> ${MAP_HTML}
-    echo "<tr><td>Omni-Bot File:</td><td><a href=${MAP_BOTS}>${MAP_BOTS}</a></td></tr>" >> ${MAP_HTML}
+    echo "        <tr><td>Map File:</td><td><a href=${MAP_FILE}>${MAP_FILE}</a></td></tr>" >> ${MAP_HTML}
+    echo "        <tr><td>Omni-Bot File:</td><td><a href=${MAP_BOTS}>${MAP_BOTS}</a></td></tr>" >> ${MAP_HTML}
   fi
-  echo "<tr><td colspan=2 align=center><br>Redstar:ET</a></td></tr>" >> ${MAP_HTML}
-  echo "</table>" >> ${MAP_HTML}
-  echo "</center>" >> ${MAP_HTML}
+  echo "      </table>" >> ${MAP_HTML}
+  echo "      <br><a href=/etmaps>&lt;---- Return to Index</a>" >> ${MAP_HTML}
+  echo "    </p>" >> ${MAP_HTML}
+  echo "  </div>" >> ${MAP_HTML}
+  echo "  <div class=LayoutMap_ccimg><IMG SRC=${MAP_CCIMG} class="center"></div>" >> ${MAP_HTML}
+  echo "  <div class=LayoutMap_img><IMG SRC=${MAP_IMG} class="center"></div>" >> ${MAP_HTML}
+  echo "  <div class=LayoutMap_footer><br><h5>Copyright &copy; 2013 - Redstar:ET</h5></div>" >> ${MAP_HTML}
+  echo "</div>" >> ${MAP_HTML}
   echo "</body>" >> ${MAP_HTML}
   echo "</html>" >> ${MAP_HTML}
   cd ../..
