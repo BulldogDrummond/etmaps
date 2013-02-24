476c02c73c4a514e7bf9bf1fcfe3a81ae7c13761
Bulldog Drummond <butchdrummond@gmail.com>
2013-02-24 12:20:54 -0500

    Change font on index page.

--- index.php
+++ index.php
@@ -10,7 +10,11 @@
     <h2>Redstar:ET - Map Archive</h2>
   </div>
   <div class=LayoutIndex_main>
+    <p class="typewriter">
     <?php include "mapindex.inc.php"; ?>
+    <br>
+    <a href=/>&lt;---- Return to Main Index</a><br><br>
+    </p>
   </div>
   <div class=LayoutIndex_footer><br><h5>Copyright &copy; 2013 - Redstar:ET</h5></div> 
 </div>
